4ec4ab07cb9a281a8528f56b8289709100055ae6
Yusuf Yasin Kaya <yusuf2979@gmail.com>
2022-08-29 10:04:09 +0300

    ListTile wrap with Card

--- lib/screens/user_todos/view/user_todos_view.dart
+++ lib/screens/user_todos/view/user_todos_view.dart
@@ -24,42 +24,41 @@ class _UserTodosViewState extends State<UserTodosView> {
         title: const Text("My todos"),
       ),
       body: viewmodel.state==StatusDataCheck.okay?
-               ListView.separated(
+               ListView.builder(
                 padding: EdgeInsets.only(top: 15),
-                separatorBuilder: ((context, index) {
-                  return Divider(
-                    color: Colors.black,
-
-                  );
-                }),
+                
                 itemCount: viewmodel.todosModel!.todos!.length,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(viewmodel.todosModel!.todos![index].todo ?? "",style: Theme.of(context).textTheme.headline5,),
-                    subtitle: viewmodel.todosModel!.todos![index].completed == false
-                        ? const Text.rich(
-                        TextSpan(
-                          children: [
-                                 TextSpan(text: "Not Completed   "),
-                              
-                                 WidgetSpan(child: Icon(Icons.highlight_remove_rounded,color: Color.fromARGB(255, 236, 0, 0),))
-                          ]
-                        ))
-                        : const Text.rich(
-                        TextSpan(
-                          children: [
-                                 TextSpan(text: "Completed   "),
-                                 WidgetSpan(child: Icon(Icons.check_circle_rounded,color: Color.fromARGB(255, 0, 217, 7),))
-                          ]
-                        )),
-                    trailing: TextButton(
-                      onPressed: () async{
-                       bool result= await viewmodel.todosPut(viewmodel.todosModel!.todos![index].id!,
-                            !viewmodel.todosModel!.todos![index].completed!, index);
+                  return Card(
+                    shadowColor: Colors.black,
+                    elevation: 4,
+                    child: ListTile(
+                      title: Text(viewmodel.todosModel!.todos![index].todo ?? "",style: Theme.of(context).textTheme.headline5,),
+                      subtitle: viewmodel.todosModel!.todos![index].completed == false
+                          ? const Text.rich(
+                          TextSpan(
+                            children: [
+                                   TextSpan(text: "Not Completed   "),
+                                
+                                   WidgetSpan(child: Icon(Icons.highlight_remove_rounded,color: Color.fromARGB(255, 236, 0, 0),))
+                            ]
+                          ))
+                          : const Text.rich(
+                          TextSpan(
+                            children: [
+                                   TextSpan(text: "Completed   "),
+                                   WidgetSpan(child: Icon(Icons.check_circle_rounded,color: Color.fromARGB(255, 0, 217, 7),))
+                            ]
+                          )),
+                      trailing: TextButton(
+                        onPressed: () async{
+                         bool result= await viewmodel.todosPut(viewmodel.todosModel!.todos![index].id!,
+                              !viewmodel.todosModel!.todos![index].completed!, index);
 
-                            viewmodel.todosModel!.todos![index].completed=result;
-                      },
-                      child: Text("Update", style: TextStyle(color: Colors.black),),
+                              viewmodel.todosModel!.todos![index].completed=result;
+                        },
+                        child: Text("Update", style: TextStyle(color: Colors.black),),
+                      ),
                     ),
                   );
                 }
